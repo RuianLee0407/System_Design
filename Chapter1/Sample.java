0acb836837c2abcdb55b3c6ab19a5ad70ae50e30
RuianLee0407 <109220430+RuianLee0407@users.noreply.github.com>
2022-07-16 05:57:54 +0800

    Create Circle
    To accomplish the homework of Chapter 1

---
+++ Chapter1/Sample.java
@@ -0,0 +1,12 @@
+package Chapter1;
+
+import shape.Circle;
+
+class Sample{
+    public static void main(String[] args) {
+        Circle c1 = new Circle();
+        c1.setValue();
+        c1.setValue2();
+    }
+
+}
---
+++ shape/Circle.java
@@ -0,0 +1,48 @@
+package shape;
+
+import java.util.Scanner;
+
+public class Circle {
+    // instance variable    
+    int x1;
+    int y1;
+    int radius;
+    final String CHS_NAME = "圓形";
+    // public to make other program use this method to create object
+    // public Circle(int x1, int y1, int radius){
+    //     this.x1 = x1;
+    //     this.y1 = y1;
+    //     this.radius = radius;
+    // }
+    private String next;
+    
+    public void show(){
+        System.out.printf(CHS_NAME + "在" + "(%d , %d)的座標位置顯示。\n",this.x1,this.y1);
+        System.out.printf("半徑大小為%d \n",this.radius);
+    }
+
+    public void setValue(){
+        Scanner cin = new Scanner(System.in);
+        System.out.printf("請輸入x座標\n");
+        this.x1 = cin.nextInt();
+        System.out.printf("請輸入y座標\n");
+        this.y1 = cin.nextInt();
+        System.out.printf("請輸入半徑\n");
+        this.radius = cin.nextInt();
+        this.show();
+    }
+    public void setValue2(){
+        Scanner cin = new Scanner(System.in);
+        String text = cin.nextLine();
+        cin.close();
+        String[] tokens = text.split(" ");
+        int values[] = new int[tokens.length];
+        for(int i=0;i<tokens.length;i++){
+            values[i] = Integer.parseInt(tokens[i]);
+        }
+        this.x1 = values[0];
+        this.y1 = values[1];
+        this.radius = values[2];
+        this.show();
+    }
+}
